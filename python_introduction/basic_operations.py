dd2c7596e2c3f4b2d8810a47f5f54bbea35afd34
piero25xx <pierremhanna3@gmail.com>
2024-06-22 20:26:16 +0300

    committing first python code

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+x = 5
+y = 10 
+
+sum = x + y 
+
+print ("The addition of x and y is: ", sum) 
+
+sub = y - x
+
+print ("The subtraction of y and x is: ", sub)
+
+
+multi = x * y
+
+print ("The Multiplication of x and y is: ", multi)
+
