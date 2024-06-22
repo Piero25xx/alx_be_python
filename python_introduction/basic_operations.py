3cf0b8b56679d1b4fa98e41c0897c9fea744a547
piero25xx <pierremhanna3@gmail.com>
2024-06-22 20:32:28 +0300

    editing my code

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,16 +1,16 @@
-number 1 = 5
-number 2 = 10 
+number 1 = 10
+number 2 = 5 
 
 sum = x + y 
 
-print ("The addition of x and y is: ", sum) 
+print ("The addition of number 1 and number 2 is: ", sum) 
 
 sub = y - x
 
-print ("The subtraction of y and x is: ", sub)
+print ("The subtraction of number 1 and number 2 is: ", sub)
 
 
 multi = x * y
 
-print ("The Multiplication of x and y is: ", multi)
+print ("The Multiplication of number 1 and number 2 is: ", multi)
 
