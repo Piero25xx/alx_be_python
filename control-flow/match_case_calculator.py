bc7214d36269b48b8e5d56aad857d1a3db4d6d8a
Pierre Mikhael <85271778+Piero25xx@users.noreply.github.com>
2024-06-29 00:13:25 +0300

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -5,17 +5,17 @@ operation = input ("Choose the operation (+, -, *, /): ")
 match operation:
 
     case '+':
-        print ("The result is ",x+y,".")
+        print(f"the result is {x+y}")
 
     case '-':
-         print ("The result is ",x-y,".")
+         print(f"the result is {x+y}")
 
     case "/":
          if (y == 0):
              print ("Cannot divide by zero.")
 
          else:
-             print ("The result is ",x/y,".")
+             print(f"the result is {x+y}")
 
     case "*":
-          print ("The result is ",x*y,".")
+          print(f"the result is {x+y}")
