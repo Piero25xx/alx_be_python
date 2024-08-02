befb2ed3701f52a1e30f215a630d8e217f95c405
piero25xx <pierremhanna3@gmail.com>
2024-08-02 13:44:52 +0300

    book class app

---
+++ oop/book_class.py
@@ -0,0 +1,23 @@
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+        print(f"Creating {self.title}")
+
+    def __del__(self):
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
+# Testing the implementation
+if __name__ == "__main__":
+    my_book = Book("1984", "George Orwell", 1949)
+    print(my_book)  # Should use __str__
+    print(repr(my_book))  # Should use __repr__
+    del my_book  # Should trigger __del__
+
